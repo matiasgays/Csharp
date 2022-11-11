115c00b08bc9eb1b67a89dfc97ed05b86af20fc8
Matias Gays <ing.matiasgays@gmail.com>
2022-11-11 16:58:11 -0300

    Add project files.

---
+++ aLaCarta/Program.cs
@@ -0,0 +1,44 @@
+﻿using aLaCarta;
+using Utilities;
+
+var t = new TablePrinter("Seccion", "Producto", "Precio");
+
+var carta = new Dictionary<string, Dictionary<string, double>>();
+
+carta.Add("Bebidas Sin Alcohol", new Dictionary<string, double>(){
+    { "JUGOS", 90 },
+    { "GASEOSAS", 85 },
+    { "AGUA", 95 }
+});
+carta.Add("Bebidas Alcoholicas", new Dictionary<string, double>(){
+    { "DAIKIRIS", 91 },
+    { "VINOS", 86 },
+    { "CERVEZA", 96 }
+});
+carta.Add("Entrada", new Dictionary<string, double>(){
+    { "PICADA PARA 2", 88 },
+    { "SANDWICHES DE MIGA", 83 },
+    { "EMPANADAS DE CARNE", 93 }
+});
+carta.Add("Platos", new Dictionary<string, double>(){
+    { "PAELLA", 85 },
+    { "LOMO A LA PIMIENTA", 83 },
+    { "MERLUZA CON PURE", 93 }
+});
+carta.Add("Postres", new Dictionary<string, double>(){
+    { "BUDIN DE PAN", 85 },
+    { "FLAN CON CREMA", 83 },
+    { "DURAZNOS EN ALMIBAR", 93 }
+});
+
+foreach (var kvp in carta)
+{
+    t.AddRow(kvp.Key,"","");
+    foreach (var key in kvp.Value)
+    {
+        t.AddRow("",key.Key,key.Value);
+    }
+}
+
+t.Print();
+
---
+++ aLaCarta/plato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aLaCarta
+{
+    public class Plato
+    {
+        public string Nombre { get; set; }
+
+
+    }
+}
