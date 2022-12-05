c82fbbc0f2863cdb3c93442bf853abd5cefccf93
matiasgays <66641926+matiasgays@users.noreply.github.com>
2022-12-05 21:58:33 +0000

    Incorporacion WebAPI

---
+++ WebAPI/WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Repositories;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet("{userId}")]
+        public ActionResult<List<Product>>? Get(long userId)
+        {
+            try
+            {
+                return Ok(ADO_Product.SelectProduct(userId));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>>? Get()
+        {
+            try
+            {
+                return Ok(ADO_Product.SelectProducts());
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] long id)
+        {
+            try
+            {
+                int rowsAffected = ADO_Product.DeleteProduct(id);
+                if (rowsAffected > 0)
+                    return Ok($"Rows affected: {rowsAffected}");
+                return NotFound("Id not found");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Product product)
+        {
+            try
+            {
+                int rowsAffected = ADO_Product.InsertProduct(product);
+                if (rowsAffected > 0)
+                    return Ok($"Rows affected: {rowsAffected}");
+                return NotFound("Id not found");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Product product)
+        {
+            try
+            {
+                int rowsAffected = ADO_Product.UpdateProduct(product);
+                if (rowsAffected > 0)
+                    return Ok($"Rows affected: {rowsAffected}");
+                return NotFound("Id not found");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
